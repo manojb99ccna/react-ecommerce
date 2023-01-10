90c812794502ddd3a40d827fae5a6076d01807cc
manojb99ccna <manojb99.ccna@gmail.com>
2023-01-10 12:57:19 +0530

    add payment rozerpay

--- src/Events/Endpoint.js
+++ src/Events/Endpoint.js
@@ -3,6 +3,7 @@ export const HOST = 'https://react.webatsolution.com/';
 export const Endpoint = {
     GET_PRODUCT_LISTING: HOST + "/wp-json/wc/v3/products",
     GET_CATEGORY_LISTING_FILTER: HOST + "/wp-json/wc/v3/products/categories",
+    ORDER_SUBMIT: HOST + "/wp-json/wc/v3/orders",
     SUBMIT_CONTACT_FORM: HOST + "/wp-json/contact-form-7/v1/contact-forms/2190/feedback",
 
     HOME_SLIDER_LIST: HOST + "/wp-json/react/v1/slider",
